301b71971dd8f63e783aa7e23b28032932668422
unknown <emgcalangian@gmail.com>
2024-01-20 18:28:14 +0800

    getting the sum of two numbers

--- return_product_or_sum_of_twonumbers.py
+++ return_product_or_sum_of_twonumbers.py
@@ -11,6 +11,9 @@ second_number = int(input("Enter the second number: "))
 # Get the product of the two numbers
 product = first_number * second_number
 print(product)
+
 # Add the sum of two numbers
+sum = first_number + second_number
+print(sum)
 # Write it so that you'll return the product of the two numbers if equal to or lower than 1000 otherwise return their sum
 
