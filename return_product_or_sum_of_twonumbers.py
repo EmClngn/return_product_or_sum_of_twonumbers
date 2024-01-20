263e78deadf64bbe5eb16d6c8682fa4fdfb401e3
unknown <emgcalangian@gmail.com>
2024-01-20 18:16:05 +0800

    psuedo code

---
+++ return_product_or_sum_of_twonumbers.py
@@ -0,0 +1,8 @@
+# Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+# Psuedo Code
+# Ask user input for 2 numbers
+# Get the product of the two numbers
+# Add the sum of two numbers
+# Write it so that you'll return the product of the two numbers if equal to or lower than 1000 otherwise return their sum
+
