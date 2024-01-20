9ba6f4b685cf3779718f807e9ec4e8db57604d89
unknown <emgcalangian@gmail.com>
2024-01-20 18:35:41 +0800

    fulfilling the requirements(return if product is equal to or lower than 100. Return the sum if not)

--- return_product_or_sum_of_twonumbers.py
+++ return_product_or_sum_of_twonumbers.py
@@ -10,10 +10,14 @@ second_number = int(input("Enter the second number: "))
 
 # Get the product of the two numbers
 product = first_number * second_number
-print(product)
+
 
 # Add the sum of two numbers
 sum = first_number + second_number
-print(sum)
-# Write it so that you'll return the product of the two numbers if equal to or lower than 1000 otherwise return their sum
 
+
+# Write it so that you'll return the product of the two numbers if equal to or lower than 1000 otherwise return their sum
+if product <= 1000: 
+    print(product)
+else:
+    print(sum)
